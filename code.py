d618d16d2c4ad718dd94ed6b57dd29d71c5695d7
Szymon Jakubiak <sz.a.jakubiak@gmail.com>
2022-07-06 08:55:24 +0200

    Select song using RFID tag uid

--- code.py
+++ code.py
@@ -26,23 +26,21 @@ dfplayer_busy.direction = Direction.INPUT
 
 dfplayer = DFPlayer(uart, volume=dfplayer_vol)
 
+songs_uid = {"0x79aae7a3" : 1, "0x397358c1" : 2, "0x69bb5ec2" : 3}
 
 while True:
-    (stat, tag_type) = rfid.request(rfid.REQIDL)
-    
-    if stat == rfid.OK:
-        (stat, raw_uid) = rfid.anticoll()
+    if dfplayer_busy.value:
+        (stat, tag_type) = rfid.request(rfid.REQIDL)
+        
         if stat == rfid.OK:
-            uid = "0x%02x%02x%02x%02x" % (raw_uid[0], raw_uid[1], raw_uid[2], raw_uid[3])
-            print("uid: ", uid)
-            print("")
+            (stat, raw_uid) = rfid.anticoll()
+            if stat == rfid.OK:
+                uid = "0x%02x%02x%02x%02x" % (raw_uid[0], raw_uid[1], raw_uid[2], raw_uid[3])
+                print("uid: ", uid)
+                if uid in songs_uid.keys():
+                    dfplayer.play(cd, songs_uid[uid])
+                    print("Playing song ", songs_uid[uid])
+                print("")
     elif not btn.value:
         print("Button pressed")
-
-    #song = randint(1, songs_count[cd])
-    #dfplayer.play(cd, song)
-
-    #while not dfplayer_busy.value:
-    #    pass
-    
-    #sleep(2)
+        dfplayer.stop()
